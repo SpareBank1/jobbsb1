61067ba6eff816ca36172e75aa56de802513d90b
Anders <anders.johnsen@sparebank1.no>
2019-01-24 14:37:52 +0100

    Add .npmrc

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const Header = ({ siteTitle }) => (
-  <div>
+  <header>
     <div>
       <h1 className="ffe-h1">
         <Link
@@ -13,7 +13,7 @@ const Header = ({ siteTitle }) => (
         </Link>
       </h1>
     </div>
-  </div>
+  </header>
 )
 
 Header.propTypes = {
