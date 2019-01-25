fb21541fbcb6d7296d2498fe534308b013c2d1ea
andersjohnsen <anders.johnsen@sparebank1.no>
2019-01-25 14:07:10 +0100

    Updates

--- component---src-pages-page-2-js-9411fb831bda119fe82f.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{140:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(143),c=a(145),r=a(144);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-9411fb831bda119fe82f.js.map
