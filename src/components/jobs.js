ce6746bb17c7792d8041af819ad171e9a73eb90b
Camilla Lund <camilla.lund@sparebank1.no>
2019-02-28 17:10:42 +0100

    Styling for jobblisten

--- src/components/jobs.js
+++ src/components/jobs.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
+import { ShortcutButton } from '@sb1/ffe-buttons-react';
+
 
 export default () => (
-
+<div className="sb1-hrmanager">
     <StaticQuery
       query={graphql`
         query hrQuery{
           allHRmanagerJob(
-            limit: 25,
+            limit: 5,
             sort:{
               fields:LastUpdated,
               order:DESC
@@ -30,11 +32,14 @@ export default () => (
         data => (
           data.allHRmanagerJob.edges.map(post => {
             if (post.node.Name==='dummy'){
-              return <div>ingen ledige stillinger</div>
+              return (<div>
+                <ShortcutButton element="a" href="https://www.webcruiter.no/WcMain/AdvertViewPublic.aspx?oppdragsnr=4004904858&Company_Id=3670262172&Culture_Id=NB-NO&cols=3,5,7&orderby=5&listtype=1&urltarget=_blank" 
+                                    target="_blank" rel="noopener noreferrer">Utvikler? Bli med å skape morgendagens bank</ShortcutButton>
+                </div>)
             }else{
               return (
                 <div key={post.node.Id}>
-                  <a href={post.node.AdvertisementUrl} target="_blank" rel="noopener noreferrer">{post.node.Name}</a>
+                  <ShortcutButton element="a" href={post.node.AdvertisementUrl} target="_blank" rel="noopener noreferrer">{post.node.Name}</ShortcutButton>
                 </div>
               )
             }
@@ -42,5 +47,5 @@ export default () => (
         )
       )}
     />
-
+    </div>
 )
