4328713618c63a7869f0cc0d4e74505b90fa5193
Camilla Lund <camilla.lund@sparebank1.no>
2019-05-02 13:31:13 +0200

    FFE

--- src/components/mjobs.js
+++ src/components/mjobs.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-import { TextCard } from '@sb1/ffe-cards-react';
+import { TextCard } from '@sb1/ffe-cards-react'; 
 
 export default () => (
   <StaticQuery
@@ -30,8 +30,12 @@ export default () => (
           { data.allMarkdownRemark.edges.map(post => (
               <div className="sb1-joblist__item">
                 <TextCard element="a" className="sb1-joblist__item-content" key={post.node.id} href={post.node.frontmatter.path}>
-                  <h4 className="ffe-h4">{post.node.frontmatter.title}</h4>
-                  <p>{post.node.frontmatter.description}</p>
+                {({ Title, Text }) => (
+                  <React.Fragment>
+                  <Title>{post.node.frontmatter.title}</Title>
+                  <Text>{post.node.frontmatter.description}</Text>
+                  </React.Fragment>
+                )}
                 </TextCard>
               </div>   
             ))}
