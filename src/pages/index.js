026ca40458b11524f5464fd05e553e2b0500d2d9
Camilla Lund <camilla.lund@sparebank1.no>
2019-12-20 15:04:40 +0100

    Placeholder for ansatte

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ const IndexPage = (props) => (
     <Header/>
     <SEO title="Vi søker nye medarbeidere" keywords={[`sparebank 1`, `utvikling`, `react`, `frontend`, `backend`, `design`, `UX`, `IxD`]} />
 
-    <svg class="blob" viewBox="0 0 600 600" xmlns="http://www.w3.org/2000/svg">
-        <g transform="translate(300,300)">
+    <svg class="blob" viewBox="0 0 500 400" xmlns="http://www.w3.org/2000/svg">
+        <g transform="translate(400,100)">
             <path d="M120,-157.6C152.7,-141.5,174.3,-102.6,194.8,-58.8C215.3,-14.9,234.6,33.8,228.4,80.8C222.2,127.8,190.4,173.1,148.1,184C105.8,195,52.9,171.5,-2.4,174.8C-57.8,178.2,-115.6,208.4,-137.5,190.9C-159.3,173.3,-145.3,108,-153,56.3C-160.7,4.6,-190.2,-33.4,-178.3,-54.2C-166.4,-75.1,-113.2,-78.8,-76.6,-93.6C-40,-108.3,-20,-134.2,11.9,-150.5C43.7,-166.8,87.4,-173.6,120,-157.6Z" fill="#005aa4" />
         </g>
     </svg>
@@ -40,6 +40,19 @@ const IndexPage = (props) => (
         </div>
     </div>
 
+    <Grid className="sb1-employees">
+        <GridRow>
+            <GridCol sm={{ cols: 12 }} md={{ cols: 10, offset: 1 }}>
+                <InlineGrid>
+                    <GridRow>
+                        <GridCol sm={{ cols: 12 }} md={{ cols: 6 }} lg={{ cols: 3 }}>
+                           ansatte
+                        </GridCol>
+                    </GridRow>
+                </InlineGrid>
+            </GridCol>
+        </GridRow>
+    </Grid>
 
     <Grid>
         <GridRow topPadding={ true }>
