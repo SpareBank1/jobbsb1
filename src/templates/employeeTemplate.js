38cbed8fa20b4deef82afb118fbcbc89132d3fec
Camilla Lund <camilla.lund@sparebank1.no>
2020-01-30 15:29:54 +0100

    Ansattside med blobanimasjon

--- src/templates/employeeTemplate.js
+++ src/templates/employeeTemplate.js
@@ -3,6 +3,8 @@ import { graphql } from "gatsby"
 import Layout from '../components/layout'
 import Header from '../components/header'
 import SEO from '../components/seo'
+import { Grid, GridRow, GridCol } from '@sb1/ffe-grid-react'
+import Blob from '../components/blob/blob'
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -13,22 +15,31 @@ export default function Template({
     <Layout>
       <Header/>
         <SEO title={frontmatter.title} keywords={[`sparebank 1`, `karriere`, `stilling`, `utvikling`, `design`]} />
-        <div className="container">
-        <div className="post">
-            <h1>{frontmatter.title}</h1>
-            <h2>{frontmatter.rolle}</h2>
-            <div
-            className="content"
-            dangerouslySetInnerHTML={{ __html: html }}
-            />
-        </div>
-        </div>
+        
+        <Grid className="sb1-markdown-employee">
+          <GridRow className="sb1-markdown-employee__layout">
+            <GridCol sm={{ cols: 12 }} md={{ cols: 6, offset:1  }} lg={{ cols: 6, offset:1 }}>
+              <h1 className="ffe-h2">{frontmatter.title}</h1>
+              <p>{frontmatter.rolle}</p>
+              <div
+              className="content"
+              dangerouslySetInnerHTML={{ __html: html }}
+              />
+            </GridCol>
+            <GridCol sm={{ cols: 12 }} md={{ cols: 4 }} lg={{ cols: 4 }}>
+              {frontmatter.image && <img src={frontmatter.image.childImageSharp.fixed.src} alt={frontmatter.title} className="sb1-markdown-employee__image" />}
+            </GridCol>
+          </GridRow>
+          <Blob color={'sand'} />
+        </Grid>
+
     </Layout>
   )
 }
 
 export const pageQuery = graphql`
   query($path: String!) {
+
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
@@ -36,6 +47,13 @@ export const pageQuery = graphql`
         date
         title
         rolle
+        image {
+          childImageSharp {
+            fixed(width: 640, height: 640) {
+              src
+            }
+          }
+        }
       }
     }
   }
