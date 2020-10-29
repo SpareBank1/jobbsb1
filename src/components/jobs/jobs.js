370e99dc02345dce44e92c3980441ad74082b643
Camilla Lund <camilla.lund@sparebank1.no>
2020-10-29 09:41:07 +0100

    Ny avdeling lagt til

--- src/components/jobs/jobs.js
+++ src/components/jobs/jobs.js
@@ -3,14 +3,14 @@ import { StaticQuery, graphql } from 'gatsby';
 import Job from './job';
 
 export default () => (
-
+//avd: Design, Digitalbankutvikling ansatte, Arkitektur og teknologi
     <StaticQuery
       query={graphql`
       query hrQuery {
         allHRmanagerJob(
           limit: 20, 
           sort: {fields: Created, order: DESC}, 
-          filter: {Department: {Id: {in: [21149,21110]}}}) 
+          filter: {Department: {Id: {in: [21149,21110,21136]}}}) 
           {
           totalCount
           edges {
