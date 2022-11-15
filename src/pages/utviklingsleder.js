6005d98fd2a6c398f66572698cf605e7b4b46874
Anders <anders.johnsen@gmail.com>
2022-11-15 14:28:27 +0100

    Legger til url redirects til to annonser

---
+++ src/pages/utviklingsleder.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+    
+class Utviklingsleder extends Component {
+    componentDidMount() {
+        window.location.replace("https://candidate.hr-manager.net/ApplicationInit.aspx?cid=1889&ProjectId=146052&DepartmentId=21887&MediaId=4737");
+    }
+
+    render() {
+        return <div />
+    }
+}
+
+export default Utviklingsleder
---
+++ src/pages/utviklingsledertrondheim.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+    
+class UtviklingslederTrondheim extends Component {
+    componentDidMount() {
+        window.location.replace("https://candidate.hr-manager.net/ApplicationInit.aspx?cid=1889&ProjectId=146069&DepartmentId=21887&MediaId=4737");
+    }
+
+    render() {
+        return <div />
+    }
+}
+
+export default UtviklingslederTrondheim
