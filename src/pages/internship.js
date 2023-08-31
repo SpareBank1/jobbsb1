dc559e469a4ee4d7c67edb645ba8f585acf85207
Anders <anders.johnsen@gmail.com>
2023-08-31 15:45:17 +0200

    Legger til redirect til internship-annonse

---
+++ src/pages/internship.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+    
+class Internship extends Component {
+    componentDidMount() {
+        window.location.replace("https://candidate.hr-manager.net/ApplicationInit.aspx?cid=1889&ProjectId=146825&DepartmentId=21856&MediaId=4737");
+    }
+
+    render() {
+        return <div />
+    }
+}
+
+export default Internship
