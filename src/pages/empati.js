aa6de596892fc8ef0fc437c8064b69e4e1bbcfda
kariannekristiansen <karianne.kristiansen@sparebank1.no>
2023-12-18 12:06:43 +0100

    Gjøre om fra text til json

--- src/pages/empati.js
+++ src/pages/empati.js
@@ -5,25 +5,26 @@ import sparebank1 from './empati/images/sparebank1.svg'
 import mann from './empati/images/mann.svg'
 import dame1 from './empati/images/dame1.svg'
 import dame2 from './empati/images/dame2.svg'
-import txt from './empati/empati.text'
-import createCard from './empati/components/card/CreateCard'
+import empatiJson from './empati/empati.json'
+import textCardsJson from './empati/components/card/textCards.json'
+import Cards from "./empati/components/card/Cards";
 
 const EmpatiPage = () => (
   <div className="sb1-page empati-page">
     <img src={sparebank1} alt="SpareBank 1 Logo" />
-    <h1 className="ffe-h1--text-center empati-heading">{txt.HEADING}</h1>
-    <Grid >
-      <GridRow>
-        <GridCol sm={12} lg={{ cols: 6, offset: 3 }}>
-          <p className="ffe-body-paragraph empati-paragraph">
-            {txt.PARAGRAPH_1}
-          </p>
-          <p className="ffe-body-paragraph empati-paragraph">
-            {txt.PARAGRAPH_2}
-          </p>
-        </GridCol>
-      </GridRow>
-    </Grid>
+      <h1 className="ffe-h1--text-center empati-heading">{empatiJson.heading}</h1>
+      <Grid >
+          <GridRow>
+              <GridCol sm={12} lg={{ cols: 6, offset: 3 }}>
+                  <p className="ffe-body-paragraph empati-paragraph">
+                      {empatiJson.firstParagraph}
+                  </p>
+                  <p className="ffe-body-paragraph empati-paragraph">
+                      {empatiJson.secondParagraph}
+                  </p>
+              </GridCol>
+          </GridRow>
+      </Grid>
 
     <div className="empati-images">
       <img src={mann} alt="" />
@@ -34,13 +35,20 @@ const EmpatiPage = () => (
     <Grid>
       <GridRow topPadding={false}>
         <GridCol>
-          {createCard('STAND_1', 'fjell')}
-          {createCard('STAND_2', 'syrin')}
-          {createCard('STAND_3', 'frost')}
-          {createCard('STAND_4', 'sand', true)}
-          {createCard('STAND_5', 'fjell')}
-          {createCard('STAND_6', 'syrin')}
-          {createCard('STAND_7', 'sand')}
+            {textCardsJson.map((card, index) => (
+                   <Cards
+                       key={index}
+                       hands={card.hands}
+                       color={card.color}
+                       title={card.heading}
+                       subTitle={card.tasks}
+                       text={card.text}
+                       task1={card.task1}
+                       task2={card.task2}
+                       task3={card.task3}
+                   />
+               ))
+            }
         </GridCol>
       </GridRow>
     </Grid>
