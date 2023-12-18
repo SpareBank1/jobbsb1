6e601fddb75a6b42dd0a06df57dc5fc571ffc8b1
kariannekristiansen <karianne.kristiansen@sparebank1.no>
2023-12-18 11:06:52 +0100

    Ramme rundt empati-siden

--- src/pages/empati.js
+++ src/pages/empati.js
@@ -1,18 +1,50 @@
-import React from 'react';
-
-import {createCard} from "./empati/components/card/CreateCard";
+import React from 'react'
+import './empati/empati.less'
+import { Grid, GridRow, GridCol } from '@sb1/ffe-grid-react'
+import sparebank1 from './empati/images/sparebank1.svg'
+import mann from './empati/images/mann.svg'
+import dame1 from './empati/images/dame1.svg'
+import dame2 from './empati/images/dame2.svg'
+import txt from './empati/empati.text'
+import { createCard } from './empati/components/card/CreateCard'
 
 const EmpatiPage = () => (
-    <>
-    <div>Empati</div>
-        {createCard('STAND_1', 'fjell')}
-        {createCard('STAND_2', 'syrin')}
-        {createCard('STAND_3', 'frost')}
-        {createCard('STAND_4', 'sand', true)}
-        {createCard('STAND_5', 'fjell')}
-        {createCard('STAND_6', 'syrin')}
-        {createCard('STAND_7', 'sand')}
-    </>
-);
+  <div className="sb1-page empati-page">
+    <img src={sparebank1} alt="SpareBank 1 Logo" />
+    <h1 className="ffe-h1--text-center empati-heading">{txt.HEADING}</h1>
+    <Grid >
+      <GridRow>
+        <GridCol sm={12} lg={{ cols: 6, offset: 3 }}>
+          <p className="ffe-body-paragraph empati-paragraph">
+            {txt.PARAGRAPH_1}
+          </p>
+          <p className="ffe-body-paragraph empati-paragraph">
+            {txt.PARAGRAPH_2}
+          </p>
+        </GridCol>
+      </GridRow>
+    </Grid>
 
-export default EmpatiPage;
+    <div className="empati-images">
+      <img src={mann} alt="" />
+      <img src={dame2} alt="" className="empati-images image--shrunk" />
+      <img src={dame1} alt="" />
+    </div>
+
+    <Grid>
+      <GridRow topPadding={false}>
+        <GridCol>
+          {createCard('STAND_1', 'fjell')}
+          {createCard('STAND_2', 'syrin')}
+          {createCard('STAND_3', 'frost')}
+          {createCard('STAND_4', 'sand', true)}
+          {createCard('STAND_5', 'fjell')}
+          {createCard('STAND_6', 'syrin')}
+          {createCard('STAND_7', 'sand')}
+        </GridCol>
+      </GridRow>
+    </Grid>
+  </div>
+)
+
+export default EmpatiPage
---
+++ src/pages/empati/empati.text.js
@@ -0,0 +1,8 @@
+const txt = {
+  HEADING: 'Velkommen til empati-laben!',
+  PARAGRAPH_1:
+    '11. januar forvandles minglegangen i H9 1. etasje til en empati-lab. Kom og få et inntrykk av hvordan det er å ha for eksempel grå stær, skjelving i hendene eller netthinneløsning',
+  PARAGRAPH_2: 'Under kan du lese om de 7 ulike stasjonene du kan besøke',
+}
+
+export default txt
